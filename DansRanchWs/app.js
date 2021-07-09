2a215b94f37612a9eeaf8fc950767dc0e69f80c2
DanTrinh685 <38805001+DanTrinh685@users.noreply.github.com>
2021-07-09 16:01:58 -0500

    Update app.js

--- DansRanchWs/app.js
+++ DansRanchWs/app.js
@@ -3,13 +3,11 @@ var path = require('path');
 const MongoClient = require("mongodb").MongoClient
 var db;
 var app = express();
+require('dotenv/config');
 
 MongoClient.connect("mongodb+srv://admin:dantrinh@dansranch.bga8u.mongodb.net/Inventory?retryWrites=true&w=majority",{useUnifiedTopology:true, useNewUrlParser:true, useCreateIndex:true}, function(err, database) {
     if (err) return console.log(err);
     db = database.db("Inventory");
-    app.listen(3000, function(){
-        console.log("listening to port 3000")
-    })
 })
 app.get("/", function (req, res) {
     // console.log("hello I'm on the start page");
@@ -32,3 +30,9 @@ app.get("/sleeves", function(req,res){
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get("port"), function(){
+    console.log("Server started on port " + app.get("port"));
+})
